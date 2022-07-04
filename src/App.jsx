6309ccfb1d85958564bacf63fd1ffc29382c1bae
imkarimkarim <allalirezamail@gmail.com>
2022-07-04 21:29:29 +0430

    add chakra-ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div className="App">Hello World</div>;
+    </ChakraProvider>
+  );
+}
+
+export default App;
