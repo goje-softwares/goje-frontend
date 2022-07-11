a278a16afb7b254401fa5a6dd05575f638343af9
karim <43340560+imkarimkarim@users.noreply.github.com>
2022-07-11 13:30:59 +0430

    add router (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">Hello World</div>;
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </ChakraProvider>
   );
 }
