dc2e391007b026159de9bbdf31bed04578d117b7
imkarimkarim <allalirezamail@gmail.com>
2022-07-11 15:27:37 +0430

    add some color pallete

--- src/Global/theme.jsx
+++ src/Global/theme.jsx
@@ -5,6 +5,13 @@ const theme = extendTheme({
     heading: `'Vazirmatn', sans-serif`,
     body: `'Vazirmatn', sans-serif`,
   },
+  colors: {
+    lightGray: "#FAF8F8",
+    tomato: "#CD1624",
+    tomatoLight: "#EC4A57",
+    tomatoVeryLight: "#F4929A",
+    grass: "#006F57",
+  }
 });
 
 export default theme;
