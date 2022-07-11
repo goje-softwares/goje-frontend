e6685c65d3f39e0c5e3a345b290d606a28550c71
karim <43340560+imkarimkarim@users.noreply.github.com>
2022-07-11 13:47:08 +0430

    add Vazirmatn font (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
+import theme from "./Global/theme";
 import Home from "./Pages/Home";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
