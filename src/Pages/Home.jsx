7ea18af0e5b21a1997215cb804181bf4f1022ac6
imkarimkarim <allalirezamail@gmail.com>
2022-07-11 17:12:48 +0430

    adjustmenting margins

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,12 +3,12 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box mt="40" textAlign={"center"}>
+    <Box mt="140px" textAlign={"center"}>
       <Heading fontSize="5xl" color="tomato">
         گـوجـه
       </Heading>
-      <Text mt={"5"}>به زودی...</Text>
-      <Text mt={"10"}>
+      <Text mt={"20px"}>به زودی...</Text>
+      <Text mt={"40px"} mr={"30px"} ml={'30px'}>
         گوجه خودشم هنوز دقیق نمیدونه چیه ولی قراره تو حساب کتابات بهت کمک کنه ;)
       </Text>
     </Box>
