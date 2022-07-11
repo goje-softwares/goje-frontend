9b0e49a03de949cb6bd9ecb336f41aaad4e2ec36
imkarimkarim <allalirezamail@gmail.com>
2022-07-11 17:07:23 +0430

    add: Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
-  return <Box>به گوجه خوش آمدید</Box>;
+  return (
+    <Box mt="40" textAlign={"center"}>
+      <Heading fontSize="5xl" color="tomato">
+        گـوجـه
+      </Heading>
+      <Text mt={"5"}>به زودی...</Text>
+      <Text mt={"10"}>
+        گوجه خودشم هنوز دقیق نمیدونه چیه ولی قراره تو حساب کتابات بهت کمک کنه ;)
+      </Text>
+    </Box>
+  );
 }
