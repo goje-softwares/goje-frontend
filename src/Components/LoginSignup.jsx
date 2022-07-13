918c3ed076b91d6a5a72431654df364cd543d4e8
imkarimkarim <allalirezamail@gmail.com>
2022-07-13 15:56:31 +0430

    updated login-signup-popup

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -5,7 +5,11 @@ import {
   ModalContent,
   useDisclosure,
   ModalBody,
+  ModalCloseButton,
   Box,
+  Link,
+  InputGroup,
+  InputLeftElement,
   Button,
   Tabs,
   TabList,
@@ -13,13 +17,20 @@ import {
   TabPanel,
   TabPanels,
   HStack,
+  VStack,
+  Input,
+  Text,
 } from "@chakra-ui/react";
+import { ViewIcon } from "@chakra-ui/icons";
 
 export default function LoginSignup() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
+
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
 
   return (
     <>
@@ -36,7 +47,8 @@ export default function LoginSignup() {
       >
         <ModalOverlay />
         <ModalContent>
-          <ModalBody pt="5">
+          <ModalCloseButton />
+          <ModalBody pt="40px">
             <Tabs colorScheme="green" isFitted>
               <TabList>
                 <Tab>ورود</Tab>
@@ -45,11 +57,43 @@ export default function LoginSignup() {
               <TabPanels>
                 <TabPanel pl="0" pr="0">
                   <Box>
-                    <Box></Box>
+                    <Box pt={"10px"} pb={"14px"}>
+                      <Text textAlign={"center"} fontSize="sm" color="gray">
+                        لطفا نام کاربری و رمز عبور خود را وارد کنید.
+                      </Text>
+                    </Box>
                     <Box>
-                      <HStack justify={"left"} spacing={"10px"}>
-                        <Button colorScheme="green">ورود</Button>
+                      <VStack spacing={"10px"}>
+                        <Input placeholder="نام کاربری" size="md" />
+                        <InputGroup size="md">
+                          <Input
+                            pr="15px"
+                            type={show ? "text" : "password"}
+                            placeholder="رمز ورود"
+                          />
+                          <InputLeftElement width="3.2rem">
+                            <Button h="1.75rem" size="sm" onClick={handleClick}>
+                              <ViewIcon />
+                            </Button>
+                          </InputLeftElement>
+                        </InputGroup>
+                      </VStack>
+                    </Box>
+                    <Box mt={"10px"}>
+                      <Link fontSize={"sm"}>فراموشی رمز عبور</Link>
+                    </Box>
+                    <Box pt="20px">
+                      <HStack
+                        display={"flex"}
+                        justifyContent="space-around"
+                        justify={"left"}
+                        spacing={"10px"}
+                      >
+                        <Button width={"100%"} colorScheme="green">
+                          ورود
+                        </Button>
                         <Button
+                          width={"100%"}
                           colorScheme="gray"
                           variant={"outline"}
                           onClick={onClose}
