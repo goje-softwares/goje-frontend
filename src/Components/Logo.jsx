2c693f13e60bb126faceba8288eb08ec197850eb
imkarimkarim <allalirezamail@gmail.com>
2022-07-16 16:58:36 +0430

    add icon

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Image } from "@chakra-ui/react";
+import logoFile from "../assets/logo192.png";
+
+const Logo = ({boxSize}) => {
+  return (
+    <Image boxSize={boxSize} src={logoFile} alt="Logo" />
+  );
+};
+
+export default Logo;
