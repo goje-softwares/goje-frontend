cc0b0b4754600e4c8e0e006926bbf92bcf24a98c
imkarimkarim <allalirezamail@gmail.com>
2022-08-18 21:11:08 +0430

    add cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(false);
+  });
+  it("Does not do much!", () => {
+    expect(false).to.equal(false);
+  });
+});
