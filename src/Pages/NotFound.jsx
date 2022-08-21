7a4579cf1c628c21e009feda8f02973eca8ea566
imkarimkarim <allalirezamail@gmail.com>
2022-08-21 21:28:30 +0430

    add 404

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { routes } from "../Global/Routes";
 
 export default function NotFound() {
   return (
-    <div>404</div>
-  )
+    <Box
+      display={"flex"}
+      flexDir="column"
+      alignItems={"center"}
+      justifyContent={"center"}
+      mt="160px"
+      textAlign={"center"}
+    >
+      <Heading fontSize="5xl">404</Heading>
+      <Text mt={"10px"}>صفحه مورد نظر یافت نشد</Text>
+      <Text mt={"40px"} mr={"30px"} ml={"30px"}>
+        <Link to={routes.home}>برو به صفحه اصلی</Link>
+      </Text>
+    </Box>
+  );
 }
