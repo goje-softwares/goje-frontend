dd4671d7f3a1800374c7896193a06cfa041263a8
imkarimkarim <allalirezamail@gmail.com>
2022-09-12 11:41:09 +0430

    log user out on 401 api response

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -1,4 +1,13 @@
+import store from "./store";
+
 export const isDev = () => {
   // eslint-disable-next-line no-undef
   return process.env.REACT_APP_ENV === "dev";
+};
+
+export const clearAuthLocalStorage = () => {
+  store.set("email", "");
+  store.set("name", "");
+  store.set("access_token", "");
+  store.set("token_type", "");
 };
